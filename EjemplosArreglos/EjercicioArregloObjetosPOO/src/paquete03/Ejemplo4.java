d979707b4f83f6d83fde6ab2002e6b545d741247
davidethc <121461595+davidethc@users.noreply.github.com>
2024-05-15 16:45:46 -0500

    15may4:45

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/Ejemplo4.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/Ejemplo4.java
@@ -1,31 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paquete03;
 
 import paquete01.Calificacion;
 import paquete01.Profesor;
 
-public class Ejemplo {
+public class Ejemplo4 {
 
     public static void main(String[] args) {
         // crear un arreglo de objetos de tipo Calificacion
         Calificacion[] calificaciones = new Calificacion[2];
         Profesor profesor1 = new Profesor("Franco", "nombramiento");
         Profesor profesor2 = new Profesor("Richard", "nombramiento");
-        Calificacion c = new Calificacion(10, "Computación");
-        Calificacion c2 = new Calificacion(9, "Electrónica");
+        Calificacion c = new Calificacion(10, "Computación",profesor1);
+        Calificacion c2 = new Calificacion(9, "Electrónica",profesor2);
 
         calificaciones[0] = c;
         calificaciones[1] = c2;
 
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f\n",
+            System.out.printf("%s - %.2f - Profesor (%s)\n",
                     objetoCalificacion.obtenerNombreMateria(),
-                    objetoCalificacion.obtenerNota());
+                    objetoCalificacion.obtenerNota(), objetoCalificacion.obtenerProfesor().obtenerNombre());
+            //Se esta llamando a un objeto y dentro de ese objeto tengo otro el cual tambien se llama pero se le da los parametros dentro del primero
+           ;
         }
 
     }
